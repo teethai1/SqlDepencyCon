218c3a6fc19aa3d7748e6b34b6d8235a3581254c
teethai <tharin.tee@gmail.com>
2019-11-07 08:35:30 +0700

    SqlDepencyCon Ver 1.00

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbLotno = new System.Windows.Forms.TextBox();
+            this.tbMcNo = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 44);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbLotno
+            // 
+            this.tbLotno.Location = new System.Drawing.Point(183, 54);
+            this.tbLotno.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbLotno.Name = "tbLotno";
+            this.tbLotno.Size = new System.Drawing.Size(76, 20);
+            this.tbLotno.TabIndex = 1;
+            this.tbLotno.Text = "1111A2222V";
+            // 
+            // tbMcNo
+            // 
+            this.tbMcNo.Location = new System.Drawing.Point(277, 54);
+            this.tbMcNo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbMcNo.Name = "tbMcNo";
+            this.tbMcNo.Size = new System.Drawing.Size(76, 20);
+            this.tbMcNo.TabIndex = 1;
+            this.tbMcNo.Text = "DB-ME-00";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(46, 115);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(504, 216);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 349);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tbMcNo);
+            this.Controls.Add(this.tbLotno);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbLotno;
+        private System.Windows.Forms.TextBox tbMcNo;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// How to use this program
+// 1. Change ConnectionString
+// 2. Change SQL Command for you need monitor Ex. I want to see  QCFirstLotMode, QCFinishLotMode,LotStartTime,LotEndTime
+// 3. Click button 1 for active SqlDepencyCon
+// 4. Edit data on database for checking event 
+// 5. When you edited data on database it will call dependency1_OnChange
+
+//  THARIN YENPAIROJ    07/11/2019
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection = null;
+        SqlCommand command = null;
+        DataSet dataToWatch = null;
+        string tableName1 = "DBData";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlDependencyFunction(tbLotno.Text);
+        }
+         string  GetSQLTest(string LotNo)
+        {
+            if (LotNo == "")
+            {
+                return "";
+            }
+            return "SELECT QCFirstLotMode, QCFinishLotMode,LotStartTime,LotEndTime FROM dbo.DBData WHERE Lotno = '" + LotNo + "' and MCNo = '" + tbMcNo .Text + "'";
+        }
+
+        void GetData1()
+        {
+            dataToWatch.Clear();
+            command.Notification = null;
+            SqlDependency dependency1 = new SqlDependency(command);
+            dependency1.OnChange += dependency1_OnChange;
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                adapter.Fill(dataToWatch, tableName1);
+                dataGridView1.DataSource = dataToWatch;
+                dataGridView1.DataMember = tableName1;
+
+                //เชคเงื่อนไขที่ จังหวะนี้
+            }
+            
+        }
+
+        private void dependency1_OnChange(object sender, SqlNotificationEventArgs e)
+        {
+            ISynchronizeInvoke i = (ISynchronizeInvoke)this;
+            if (i.InvokeRequired)
+            {
+                OnChangeEventHandler tempDelegate = new OnChangeEventHandler(dependency1_OnChange);
+                object[] args = new[] { sender, e };
+                i.BeginInvoke(tempDelegate, args);
+                return;
+            }
+
+            SqlDependency dependency1 = (SqlDependency)sender;
+            dependency1.OnChange -= dependency1_OnChange;
+            if (e.Type != SqlNotificationType.Change)
+                return;
+            GetData1();
+        }
+
+        private void SqlDependencyFunction(string lotNo)
+        {
+            SqlDependency.Stop(Properties .Settings.Default.SqlDepencyCon);
+            SqlDependency.Start(Properties.Settings.Default.SqlDepencyCon);
+            if (connection == null)
+                connection = new SqlConnection(Properties.Settings.Default.SqlDepencyCon);
+            command = new SqlCommand(GetSQLTest(lotNo), connection);
+            if (dataToWatch == null)
+                dataToWatch = new DataSet();
+            GetData1();
+        }
+    }
+}
